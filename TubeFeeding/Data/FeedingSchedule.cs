ac50d6db620d796b4cc7bea53e2f688d8701e352
RVNSWE <monkey_lady@hotmail.com>
2025-12-23 23:38:23 +0000

    Added list of feeding times and list of hours.

--- TubeFeeding/Data/FeedingSchedule.cs
+++ TubeFeeding/Data/FeedingSchedule.cs
@@ -4,7 +4,9 @@
     {
         public FoodPageModel Food { get; set; }
         public PatientPageModel Patient { get; set; }
-        public List<string> Times { get; set; }
+        public List<string> FormattedListOfHours { get; set; }
+        public List<string> FormattedFeedingTimes { get; set; }
+        public int[] ListOfHours {  get; set; }
         public int[] FeedingTimes { get; set; }
 
         public FeedingSchedule()
@@ -12,7 +14,9 @@
             Food = App.SchedulePages?.SelectedFood;
             Patient = App.SchedulePages?.SelectedPatient;
 
-            Times = [];
+            FormattedListOfHours = [];
+            FormattedFeedingTimes = [];
+            ListOfHours = [];
             FeedingTimes = Globals.CalculateFeedingPlan(Patient.FoodPerDay, Patient.WaterPerDay, Patient.MaxTotalVolumePerMeal);
 
             PopulateTimes();
@@ -20,15 +24,31 @@
 
         public void PopulateTimes()
         {
-            foreach (int time in FeedingTimes)
+            int startTime = FeedingTimes.First();
+            int endTime = FeedingTimes.Last();
+            int time = startTime;
+
+            for (int i = 0; i < endTime; i++)
+            {
+                ListOfHours[i] = time;
+                time++;
+            }
+
+            CreateFormattedList(ListOfHours, FormattedListOfHours);
+            CreateFormattedList(FeedingTimes, FormattedFeedingTimes);
+        }
+
+        public void CreateFormattedList(int[] list, List<string> formattedList)
+        {
+            foreach (int time in list)
             {
                 if (time < 10)
                 {
-                    Times.Add("0" + time.ToString() + ":00");
+                    formattedList.Add("0" + time.ToString() + ":00");
                 }
                 else
                 {
-                    Times.Add(time.ToString() + ":00");
+                    formattedList.Add(time.ToString() + ":00");
                 }
             }
         }
